1508a236a2d76cc058f7c620b476f59dbe0e25ca
OderoOluoch <oderoluoch@gmail.com>
2021-11-28 15:59:59 +0300

    Done with basic read and create

--- src/main/java/io/odero/Templater/controller/StudentController.java
+++ src/main/java/io/odero/Templater/controller/StudentController.java
@@ -3,10 +3,9 @@ package io.odero.Templater.controller;
 import io.odero.Templater.model.Student;
 import io.odero.Templater.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -19,5 +18,10 @@ public class StudentController {
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
         return "New Student Added successfully";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
--- src/main/java/io/odero/Templater/service/StudentService.java
+++ src/main/java/io/odero/Templater/service/StudentService.java
@@ -2,6 +2,9 @@ package io.odero.Templater.service;
 
 import io.odero.Templater.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
