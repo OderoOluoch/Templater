06472573b2e5d7d60262c506a2d114cebbe849d3
OderoOluoch <oderoluoch@gmail.com>
2021-11-28 14:55:13 +0300

    Initial commit

---
+++ src/test/java/io/odero/Templater/TemplaterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.odero.Templater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
